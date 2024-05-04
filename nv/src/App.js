294debbe071f94618af813c18e9dfa1ca654e2db
Afroze Najam <afrozenajam@gmail.com>
2024-05-05 00:46:02 +0500

    Initial Commit

---
+++ nv/src/App.js
@@ -0,0 +1,14 @@
+import { Nav } from './Component/nav';
+import { BooksDisplay } from './Component/BooksDisplay';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav></Nav>
+      <BooksDisplay></BooksDisplay>
+    </div>
+  );
+}
+
+export default App;
---
+++ nv/src/Component/BooksDisplay.js
@@ -0,0 +1,31 @@
+import React from "react";
+import '../App.css'
+import './displaystyling.css'
+
+function BooksDisplay() {
+    return(
+        <div>
+            <div className="display">
+                <div className="sidebar">
+                <h4 className="d2ah">Books By Genres</h4>
+                <div className="row">
+                    <div className="col">
+                        <button type="button" className="btn transparent-btn d-block">Fiction</button>
+                        <button type="button" className="btn transparent-btn d-block">Detective</button>
+                        <button type="button" className="btn transparent-btn d-block">Adventure</button>
+                        <button type="button" className="btn transparent-btn d-block">Children's Stories</button>
+                        <button type="button" className="btn transparent-btn d-block">History</button>
+                        <button type="button" className="btn transparent-btn d-block">Philosophy</button>
+                    </div>
+                </div>
+                </div>
+                
+            </div>
+            <div className="bookscard">
+                
+                </div>
+        </div>
+    );
+}
+
+export {BooksDisplay}
