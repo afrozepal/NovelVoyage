a119231571d06b8e2c5c5b9ad237453753e59315
Afroze Najam <150817540+afrozepal@users.noreply.github.com>
2024-05-07 01:15:25 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://afrozepal:TOdwAM50oSoqDvHG@nv.5rxrd80.mongodb.net/NovelVoyage')
+        const conn = await mongoose.connect('mongodb+srv:/.5rxrd80.mongodb.net/NovelVoyage')
         console.log('MongoDB Connected!')
     }
     catch (error) {
